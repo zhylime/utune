236f0617a0cd711ab887c2cf0d0aa4fb8358be8d
zhylime <z.ylime@hotmail.com>
2017-05-22 16:09:11 +0800

    添加星星背景移动效果

--- src/js/modules/_doc-ready.js
+++ src/js/modules/_doc-ready.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
   $('[data-js-switch-tab]').SwitchTab();
   $('[data-js-skewing]').Skewing();
   $('[data-js-masonry]').MasonryGallery();
-  //canvas的id名，星星颜色(hsla的hue色调)，星星数量，星星半径比，星星移动范围(值越大范围越小)，星星移动速度(值越大速度越慢),星星拖尾效果(0~1值越小拖尾越明显)  
-  canvas('stars',0,100,60,2,500000,0.5);
+  
+
   setTimeout(function(){
     $('[data-js-fullpage]').FullPage();
   }, 0);
