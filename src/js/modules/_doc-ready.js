d6b1c4d4ed26ae61c5df5690f2c545171b2d068f
zhylime <z.ylime@hotmail.com>
2017-06-19 15:05:52 +0800

    added side navigation

--- src/js/modules/_doc-ready.js
+++ src/js/modules/_doc-ready.js
@@ -4,8 +4,11 @@
 $(document).ready(function() {
   
   // console.log('%c Welcome to FELab, a MullenLowe Profero product. ', 'background: #c3a90a;\ncolor: #ffffff;\nfont-size: 18px;\nfont-family: "Helvetica Neue";\nfont-weight: 300;\nline-height: 30px;\nheight: 30px;\npadding: 5px');
-  
+  // homepage navigation
   $('[data-js-top-nav]').TopNav();
+
+
+
   $('[data-js-switch-tab]').SwitchTab();
   $('[data-js-skewing]').Skewing();
   $('[data-js-masonry]').MasonryGallery();
@@ -17,6 +20,9 @@ $(document).ready(function() {
   
   // carousel on about-us page
   $('[data-js-owl-carousel]').OwlCarousel();
+
+  // side bar
+  $('[data-js-side-nav]').SideNav();
 
   setTimeout(function(){
     $('[data-js-fullpage]').FullPage();
---
+++ src/js/modules/_side-nav.js
@@ -0,0 +1,40 @@
+class SideNav extends MLP.apps.MLPModule {
+
+
+  init() {
+    super.init();
+    this.el = {
+      navBtn: this.el.target.find('.js-hamburger'),
+      menuPanel: this.el.target.find('.js-panel')
+    };
+    this.event();
+  }
+
+  event(){
+    var _this = this;
+    
+
+    this.initMenuPanel();
+
+
+    
+    
+  }
+
+  initMenuPanel(){
+    var _this = this;
+
+    var toggleLeft;
+
+
+    this.el.navBtn.on('click', function(){
+      toggleLeft = $(_this.el.menuPanel).css('left') == '-320px' ? '0' : '-320px';
+      $(this).children('div').eq(0).toggleClass('active');
+      $(_this.el.menuPanel).animate({
+        left: toggleLeft
+      });
+    });
+
+  }
+}
+$.mlpPlugin(SideNav, 'SideNav', false);
