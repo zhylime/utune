06a58f1fe82655ba6e5f82075664c7d10c663f72
Qi, Ryan (BJG-MLP) <Ryan.Qi@mullenloweprofero.com>
2017-07-10 15:36:42 +0800

    添加loading

--- build/assets/js/loading.js
+++ build/assets/js/loading.js
@@ -7,7 +7,7 @@ var _PageHeight = document.documentElement.clientHeight,
 var _LoadingTop = _PageHeight > 61 ? (_PageHeight - 61) / 2 : 0,  
     _LoadingLeft = _PageWidth > 215 ? (_PageWidth - 215) / 2 : 0;  
 //在页面未加载完毕之前显示的loading Html自定义内容  
-var _LoadingHtml = '<div id="loadingDiv" style="background-color: #61cdff;position: fixed;top: 0px;bottom: 0px;overflow:hidden;right: 0px;left: 0px;margin: auto;z-index: 1000;"><style>body{overflow: hidden}</style><img src="/assets/img/loading.gif" style="position: absolute;top: 0px;bottom: 0px;right: 0px;left: 0px;margin: auto;"></div>';
+var _LoadingHtml = '<div id="loadingDiv" style="background-color: #000;background-image:url(/assets/img/loading-bg.jpg);background-repeat:no-repeat;background-size: 100% 100%;position: fixed;top: 0px;bottom: 0px;overflow:hidden;right: 0px;left: 0px;margin: auto;z-index: 1000;"><style>body{overflow: hidden}</style><img src="/assets/img/loading-cup.gif" style="position: absolute;top: 0px;bottom: 0px;right: 0px;left: 0px;margin: auto;"></div>';
 //呈现loading效果  
 document.write(_LoadingHtml);  
   
@@ -17,7 +17,7 @@ document.write(_LoadingHtml);
 //};
   
 //监听加载状态改变  
-document.onreadystatechange = completeLoading;  
+//document.onreadystatechange = completeLoading;
   
 //加载状态为complete时移除loading效果  
 function completeLoading() {  
