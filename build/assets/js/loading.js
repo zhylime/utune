a19e2c5876eb086936abce3e617f58f826d39b0c
Qi, Ryan (BJG-MLP) <Ryan.Qi@mullenloweprofero.com>
2017-07-11 10:26:36 +0800

    更新loading js

--- build/assets/js/loading.js
+++ build/assets/js/loading.js
@@ -17,7 +17,7 @@ document.write(_LoadingHtml);
 //};
   
 //监听加载状态改变  
-//document.onreadystatechange = completeLoading;
+document.onreadystatechange = completeLoading;
   
 //加载状态为complete时移除loading效果  
 function completeLoading() {  
